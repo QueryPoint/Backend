7eb1ef8af71a64d350306d9be45c5c0e5bb31093
jujunka <yu.s.muhina@edu.mtuci.ru>
2026-06-29 23:48:41 +0300

    chore(config): настроила конфигурацию

---
+++ src/config/config.py
@@ -0,0 +1,43 @@
+from pydantic import BaseModel
+from pydantic_settings import BaseSettings
+
+class CORS(BaseModel):
+    origins: list[str] = []
+
+class Database(BaseModel):
+    postgres_username: str = ""
+    postgres_db: str = ""
+    postgres_port: int = 5432
+    postgres_host: str = ""
+    postgres_password: str = ""
+    alembic_postgres_host: str | None = None
+
+    @property
+    def async_database_url(self) -> str:
+        return f"postgresql+asyncpg://{self.postgres_username}:{self.postgres_password}@{self.postgres_host}:{self.postgres_port}/{self.postgres_db}"
+
+    @property
+    def alembic_url(self) -> str:
+        host = self.alembic_postgres_host or self.postgres_host
+        return f"postgresql+asyncpg://{self.postgres_username}:{self.postgres_password}@{host}:{self.postgres_port}/{self.postgres_db}"
+
+class S3(BaseModel):
+    ENDPOINT_URL: str = "https://storage.yandexcloud.net"
+    ACCESS_KEY_ID: str = "change-me-in-production"
+    ACCESS_SECRET_KEY: str = "change-me-in-production"
+    BUCKET_NAME: str = "query-point"
+    REGION: str = "ru-central1"
+
+class JWT(BaseModel):
+    SECRET_KEY: str = "change-me-in-production"
+    ALGORITHM: str = "HS256"
+    ACCESS_TOKEN_EXPIRE_MINUTES: int = 60
+    REFRESH_TOKEN_EXPIRE_DAYS: int = 7
+
+class Config(BaseSettings):
+    database: Database = Database()
+    cors: CORS = CORS()
+    s3: S3 = S3()
+    jwt: JWT = JWT()
+
+config = Config()
